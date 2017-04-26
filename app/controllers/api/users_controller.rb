10aabd2bc5aa4cea4a34b31f5f3da6f431894fd6
Camila Kutscher <cakutscher@uc.cl>
2017-04-26 17:05:27 -0300

    ada

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < ApplicationController
   protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+  skip_before_filter :verify_authenticity_token
 
   def index
     users = User.all
